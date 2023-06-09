ecca07b46a72d2c24d2a1ec46d5dc97cb45c52e3
Mutabazi Noble <60974514+Mnoble-19@users.noreply.github.com>
2023-06-09 23:28:00 +0300

    mongoDB basics tutorial

---
+++ mongoDB/student.js
@@ -0,0 +1,116 @@
+student = {
+    "firstName": "John",
+    "lastName": "Doe",
+    "email": "john@mysql.com",
+    "gender": "M",
+    "country": "USA",
+    "isStudentActive": true,
+    "favouriteSubjects": ["Math", "Science", "English"],
+    "totalSpentInBooks": 100.50,
+}
+
+
+students = [
+    {
+        "firstName": "John",
+        "lastName": "Doe",
+        "email": "john@mysql.com",
+        "gender": "M",
+        "country": "USA",
+        "isStudentActive": true,
+        "favouriteSubjects": ["Math", "Science", "English"],
+        "totalSpentInBooks": 100.50,
+    },
+    {
+        "firstName": "Jane",
+        "lastName": "Doe",
+        "email": "jane@mysql.com",
+        "gender": "F",
+        "country": "USA",
+        "isStudentActive": true,
+        "favouriteSubjects": ["History", "Art", "Music"],
+        "totalSpentInBooks": 75.00
+    },
+    {
+        "firstName": "Bob",
+        "lastName": "Smith",
+        "email": "bob@mysql.com",
+        "gender": "M",
+        "country": "Canada",
+        "isStudentActive": true,
+        "favouriteSubjects": ["Biology", "Chemistry", "Physics"],
+        "totalSpentInBooks": 200.00
+    },
+    {
+        "firstName": "Alice",
+        "lastName": "Johnson",
+        "email": "alice@mysql.com",
+        "gender": "F",
+        "country": "USA",
+        "isStudentActive": false,
+        "favouriteSubjects": ["English", "History"],
+        "totalSpentInBooks": 50.00
+    },
+    {
+        "firstName": "David",
+        "lastName": "Lee",
+        "email": "david@mysql.com",
+        "gender": "M",
+        "country": "USA",
+        "isStudentActive": true,
+        "favouriteSubjects": ["Math", "Physics"],
+        "totalSpentInBooks": 150.00
+    },
+    {
+        "firstName": "Emily",
+        "lastName": "Wang",
+        "email": "emily@mysql.com",
+        "gender": "F",
+        "country": "China",
+        "isStudentActive": true,
+        "favouriteSubjects": ["Math", "Chemistry"],
+        "totalSpentInBooks": 80.00
+    },
+    {
+        "firstName": "Michael",
+        "lastName": "Brown",
+        "email": "michael@mysql.com",
+        "gender": "M",
+        "country": "USA",
+        "isStudentActive": true,
+        "favouriteSubjects": ["History", "English"],
+        "totalSpentInBooks": 120.00
+    },
+    {
+        "firstName": "Sophia",
+        "lastName": "Kim",
+        "email": "sophia@mysql.com",
+        "gender": "F",
+        "country": "South Korea",
+        "isStudentActive": true,
+        "favouriteSubjects": ["Math", "Science"],
+        "totalSpentInBooks": 90.00
+    },
+    {
+        "firstName": "Daniel",
+        "lastName": "Garcia",
+        "email": "daniel@mysql.com",
+        "gender": "M",
+        "country": "Spain",
+        "isStudentActive": true,
+        "favouriteSubjects": ["Math", "Physics"],
+        "totalSpentInBooks": 100.00
+    },
+    {
+        "firstName": "Isabella",
+        "lastName": "Martinez",
+        "email": "isabella@mysql.com",
+        "gender": "F",
+        "country": "Mexico",
+        "isStudentActive": true,
+        "favouriteSubjects": ["Art", "Music"],
+        "totalSpentInBooks": 60.00
+    } 
+]
+
+    
